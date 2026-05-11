f96eb7a0fdf4855e0c64f68346bde6bce84a57db
Joël van Bodegraven <49363328+jvbnl@users.noreply.github.com>
2026-05-11 13:09:36 +0200

    Remove billing/utils.ts from main scaffold

--- src/billing/utils.ts
+++
@@ -1,7 +0,0 @@
-export const eur = (n: number) =>
-  n.toLocaleString("nl-NL", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-
-export const E = (n: number) => "€ " + eur(n);
